ae4d34c2ff72e3de634592c3f4777ce62c8d31e1
Zephyrus02 <aneeshraskar@gmail.com>
2024-03-23 22:08:33 +0530

    Fixed Flask code / ai model integration

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+
+# def getResult(image_path):
+#     img = load_img(image_path, target_size=(225,225))
+#     x = img_to_array(img)
+#     x = x.astype('float32') / 255.
+#     x = np.expand_dims(x, axis=0)
+#     predictions = model.predict(x)[0]
+#     return predictions
